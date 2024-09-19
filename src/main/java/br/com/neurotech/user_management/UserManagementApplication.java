2064bcf72753c756e7a9981ee873dab147b1992e
Luiz Almeida <luizalmeida.ads@gmail.com>
2024-09-19 13:04:53 -0300

    enable SpringDataWebSuport

--- src/main/java/br/com/neurotech/user_management/UserManagementApplication.java
+++ src/main/java/br/com/neurotech/user_management/UserManagementApplication.java
@@ -2,7 +2,11 @@ package br.com.neurotech.user_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication
 public class UserManagementApplication {
 
